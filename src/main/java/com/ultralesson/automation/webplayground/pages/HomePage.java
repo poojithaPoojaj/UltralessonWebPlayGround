a637adc715ee3a45d42ae8d12aa73604558398e9
TestVagrant <testvagrant@TestVagrants-MacBook-Pro.local>
2023-04-10 21:14:44 +0530

    Page Updated

--- src/main/java/com/ultralesson/automation/webplayground/pages/HomePage.java
+++ src/main/java/com/ultralesson/automation/webplayground/pages/HomePage.java
@@ -1,19 +1,42 @@
 package com.ultralesson.automation.webplayground.pages;
 
 import com.ultralesson.automation.webplayground.models.Item;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomePage {
-    public HomePage(WebDriver driver){
+    By searchIcon = By.cssSelector("summary[aria-label='Search']");
+    By searchBar = By.id("Search-In-Modal");
+    By searchResults = By.cssSelector("li[id^='predictive-search-option'] a");
+    By productName = By.cssSelector(".predictive-search__item-heading");
 
-    }
-    public void search(String searchItem){
 
+    WebDriver webDriver;
+
+
+    public HomePage(WebDriver webDriver) {
+        this.webDriver = webDriver;
     }
-    public List<Item> getSearchItems(){
-        return new ArrayList<Item>();
+
+    public HomePage search(String searchItem) {
+        webDriver.findElement(searchIcon).click();
+        webDriver.findElement(searchBar).sendKeys(searchItem);
+        return this;
+    }
+
+    public List<Item> getSearchItems() {
+        List<WebElement> elements = webDriver.findElements(searchResults);
+        List<Item> items = new ArrayList<>();
+        for (WebElement element : elements) {
+            String name = element.findElement(productName).getText();
+            Item item = new Item();
+            item.setName(name);
+            items.add(item);
+        }
+        return items;
     }
 }
--- src/main/java/com/ultralesson/automation/webplayground/pages/LauncherPage.java
+++ src/main/java/com/ultralesson/automation/webplayground/pages/LauncherPage.java
@@ -1,12 +1,27 @@
 package com.ultralesson.automation.webplayground.pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LauncherPage {
-    public LauncherPage(WebDriver driver){
+        private WebDriver webDriver;
 
-    }
-    public void navigateTo(String url){
+        public LauncherPage(WebDriver webDriver) {
+            this.webDriver = webDriver;
+        }
 
-    }
+
+        public boolean navigateTo(String url) {
+            webDriver.get(url);
+            return isSiteLoaded();
+        }
+
+        public boolean isSiteLoaded() {
+            return new WebDriverWait(webDriver, Duration.ofSeconds(10)).until(
+                    webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        }
+
 }
